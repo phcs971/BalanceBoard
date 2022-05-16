8d8aef041d64f0d5c88f7b824264a30a3d30e491
phcs971 <phcs.971@gmail.com>
2022-05-16 15:25:47 -0300

    Initial Commit

---
+++ BalanceBoard/BalanceBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalanceBoardApp.swift
+//  BalanceBoard
+//
+//  Created by Pedro Henrique Cordeiro Soares on 16/05/22.
+//
+
+import SwiftUI
+
+@main
+struct BalanceBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BalanceBoard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BalanceBoard
+//
+//  Created by Pedro Henrique Cordeiro Soares on 16/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
